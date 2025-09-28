757c58e35f6a7c9256a905efbd161c5bc413e9ba
平川華花 <hirakawahanaka@hirakawahanahananoMac-mini.local>
2025-09-28 19:52:50 +0900

    Particleリンク変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import Particle from "../components/Particle";
+import Particle from "../components/particle";
 import Header from "@/components/Header";
 import Navigation from "@/components/Navigation";
 import Works from "@/components/home/Works";
